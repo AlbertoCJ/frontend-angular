038c4c6e8d2d2a4a57f8c23954b96cf55db4c4bf
albertocj <albertocj88j@gmail.com>
2020-03-30 20:06:51 +0200

    componente special-utton implementado.

--- src/app/shared/buttons/special-button/special-button.component.ts
+++ src/app/shared/buttons/special-button/special-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-special-button',
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpecialButtonComponent implements OnInit {
 
+  stringClassBase = 'pointer ui-special-button';
+  stringClass = `${ this.stringClassBase } ui-special-button-info`;
+
+  @Input() btnText = '';
+  @Input() disabled = false;
+  @Input('customClass') set customClass(value: string) {
+    if (value) {
+      this.stringClass = `${ this.stringClassBase } ${ value }`;
+    }
+  }
+
+  @Output() emitClick = new EventEmitter<boolean>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClick() {
+    if (!this.disabled) {
+      this.emitClick.emit(true);
+    }
   }
 
 }
