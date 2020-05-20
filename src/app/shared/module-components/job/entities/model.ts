148ab170b06888b0633ea3f48a5e6b00a43e2625
albertocj <albertocj88j@gmail.com>
2020-05-20 21:57:33 +0200

    Agregado atributo prediction al modelo del job.

--- src/app/shared/module-components/job/entities/model.ts
+++ src/app/shared/module-components/job/entities/model.ts
@@ -4,10 +4,12 @@ export class Model {
     meta: any;
     model: string | null;
     validation: Validation;
+    prediction: number[] | null;
 
     constructor(data?: any) {
         this.meta = data && data.meta ? data.meta : null;
         this.model = data && data.model ? data.model : null;
         this.validation = data && data.validation ? new Validation(data.validation) : null;
+        this.prediction = data && data.prediction ? data.prediction : null;
     }
 }
