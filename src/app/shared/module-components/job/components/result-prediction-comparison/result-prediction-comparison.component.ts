a4e8fbd473cf02b7a2017d9b1c5d30713cda9c35
albertocj <albertocj88j@gmail.com>
2020-08-01 19:20:28 +0200

    Implementado grafica prediccion

--- src/app/shared/module-components/job/components/result-prediction-comparison/result-prediction-comparison.component.ts
+++ src/app/shared/module-components/job/components/result-prediction-comparison/result-prediction-comparison.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Job } from '../../entities/job';
+import { SelectItem } from 'primeng/api/selectitem';
+import { CapitalizePipe } from '../../../../../core/pipes/capitalize/capitalize.pipe';
+import { SplitCamelCaseToStringPipe } from '../../../../../core/pipes/split-camel-case-to-string/split-camel-case-to-string.pipe';
+import { Label, Color } from 'ng2-charts';
+import { ChartDataSets, ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-result-prediction-comparison',
@@ -8,53 +13,169 @@ import { Job } from '../../entities/job';
 })
 export class ResultPredictionComparisonComponent implements OnInit {
 
+  optionsErrors: SelectItem[];
+  selectedError: string;
+
+  optionsPosition: SelectItem[];
+  selectedPosition: number;
+
+  // Pipes
+  capitalizePipe = new CapitalizePipe();
+  splitCamelCaseToStringPipe = new SplitCamelCaseToStringPipe();
+
+  // Charts
+  lineChartLabels: Label[];
+  lineChartData: ChartDataSets[];
+  lineChartColors: Color[] = [
+    { // dark grey
+      backgroundColor: 'rgba(77,83,96,0.2)',
+      borderColor: 'rgba(77,83,96,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,1)'
+    }
+  ];
+  public lineChartLegend = false;
+  public lineChartType = 'line';
+  public lineChartOptions: ChartOptions = {
+    responsive: true
+  };
+
+
   @Input() job: Job;
 
-  constructor() { }
+  constructor() {
+    this.optionsErrors = [
+      {label: this.splitCamelCaseToStringPipe.transform(this.capitalizePipe.transform('meanAbsoluteError')),
+        value: 'meanAbsoluteError'},
+      {label: this.splitCamelCaseToStringPipe.transform(this.capitalizePipe.transform('meanPriorAbsoluteError')),
+        value: 'meanPriorAbsoluteError'},
+      {label: this.splitCamelCaseToStringPipe.transform(this.capitalizePipe.transform('relativeAbsoluteError')),
+        value: 'relativeAbsoluteError'},
+      {label: this.splitCamelCaseToStringPipe.transform(this.capitalizePipe.transform('rootMeanPriorSquaredError')),
+        value: 'rootMeanPriorSquaredError'},
+      {label: this.splitCamelCaseToStringPipe.transform(this.capitalizePipe.transform('rootMeanSquaredError')),
+        value: 'rootMeanSquaredError'},
+      {label: this.splitCamelCaseToStringPipe.transform(this.capitalizePipe.transform('rootRelativeSquaredError')),
+        value: 'rootRelativeSquaredError'},
+    ];
+    this.selectedError = this.optionsErrors[0].value;
+    this.optionsPosition = [
+      { label: '1', value: 1 }
+    ];
+    this.selectedPosition = 1;
+   }
 
   ngOnInit() {
+    this.generateChartLine(this.selectedError, this.selectedPosition);
   }
 
-  // generateData() {
-  //   // const nameError = changeError.value;
+  generateOptionsPosition(size) {
+    let options: SelectItem[];
+    options = [];
+    for (let i = 0; i < size; i++) {
+      const val = i + 1;
+      options.push({ label: `${ val }`, value: val });
+    }
 
-  //   // const label = [];
-  //   // const data = [];
+    this.optionsPosition = options;
+  }
 
-  //   let loadTotalLoop = false;
-  //   let totalLoop = 1;
-  //   let cont = 0;
+  generateChartLine(changeError: any, changePosition: any) {
+    // console.log(`${ changeError } --- ${ changePosition - 1 }`);
 
-  //   while (cont < totalLoop) {
+    const chartLineData = [];
+    let major: number;
+    let minor: number;
 
-  //     const
+    const dataAlgorithms = this.job.dataAlgorithms;
 
+    for (const key in dataAlgorithms) {
+      if (dataAlgorithms.hasOwnProperty(key)) {
+        const nameAlgorithm = key;
+        // tslint:disable-next-line: max-line-length
+        if (dataAlgorithms[nameAlgorithm].model && dataAlgorithms[nameAlgorithm].model.validation && dataAlgorithms[nameAlgorithm].model.prediction) {
 
-  //     const dataAlgorithms = this.job.dataAlgorithms;
-  //     for (const key in dataAlgorithms) {
-  //       if (dataAlgorithms.hasOwnProperty(key)) {
-  
-  //         const nameAlgorithm = key;
-  //         if (dataAlgorithms[nameAlgorithm].model.validation && dataAlgorithms[nameAlgorithm].model.prediction) {
+          // Generar la lista de posicion
+          if (this.optionsPosition.length === 1) {
+            this.generateOptionsPosition(dataAlgorithms[nameAlgorithm].model.prediction.length);
+          }
 
-  //           // Inicializar total del loop
-  //           if (!loadTotalLoop) {
-  //             totalLoop = dataAlgorithms[nameAlgorithm].model.prediction.length;
-  //             loadTotalLoop = true;
-  //           }
+          const errorValue = dataAlgorithms[nameAlgorithm].model.validation[changeError];
 
-  //           // label.push(this.splitCamelCaseToStringPipe.transform(this.capitalizePipe.transform(nameAlgorithm)));
-  //           // data.push(dataAlgorithms[nameAlgorithm].model.validation[nameError]);
-  //         }
-  //       }
-  //     }
-  //     cont++;
-  //   }
+          // Crear objeto por algoritmo
+          chartLineData.push(
+            {
+              nameAlgorithm: this.splitCamelCaseToStringPipe.transform(this.capitalizePipe.transform(nameAlgorithm)),
+              error: errorValue,
+              prediction: dataAlgorithms[nameAlgorithm].model.prediction[changePosition - 1]
+            }
+          );
 
-  //   // this.lineChartLabels = label;
-  //   // this.lineChartData = [
-  //   //   { data, label: this.splitCamelCaseToStringPipe.transform(this.capitalizePipe.transform(nameError)) }
-  //   // ];
-  // }
+          // Obtener el mayor
+          if (!major) {
+            major = errorValue;
+          } else {
+            if (errorValue > major) {
+              major = errorValue;
+            }
+          }
+
+          // Obtener el menor
+          if (!minor) {
+            minor = errorValue;
+          } else {
+            if (errorValue < minor) {
+              minor = errorValue;
+            }
+          }
+
+        }
+      }
+    }
+
+    // Ordena por prediccion
+    // tslint:disable-next-line: only-arrow-functions
+    chartLineData.sort(function(a, b) { return a.prediction - b.prediction; });
+
+    // Generar grafica
+    const label = [];
+    const data = [];
+    const dataCover = [];
+
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < chartLineData.length; i++) {
+      chartLineData[i].error = 1 - ( (chartLineData[i].error - minor) / ( major - minor ) );
+
+      label.push(`${ chartLineData[i].nameAlgorithm } - ${ chartLineData[i].prediction}`);
+      data.push(chartLineData[i].error);
+      // data2.push(chartLineData[i].error / 2); // TODO: En este array gestionar la cobertura
+    }
+
+    // Genera data cover
+    let prev = -1;
+    for (let i = 0; i < chartLineData.length; i++) {
+      if (chartLineData[i].error > prev) {
+        dataCover[i] = chartLineData[i].error;
+        prev = chartLineData[i].error;
+      }
+    }
+
+    prev = -1;
+    for (let i = chartLineData.length - 1; i >= 0; i--) {
+      if (chartLineData[i].error > prev) {
+        dataCover[i] = chartLineData[i].error;
+        prev = chartLineData[i].error;
+      }
+    }
+
+    this.lineChartLabels = label;
+    this.lineChartData = [
+      { data, lineTension: 0 },
+      { data: dataCover, lineTension: 0, spanGaps: true }
+    ];
+
+  }
 
 }
